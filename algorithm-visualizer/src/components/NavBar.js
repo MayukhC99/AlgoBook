c2bef9627a75bec5e94fc3b0a2596abf825bc9c2
SUPRATIMSAHA <supratim1703@gmail.com>
2021-07-04 02:20:30 +0530

    updated

--- algorithm-visualizer/src/components/NavBar.js
+++ algorithm-visualizer/src/components/NavBar.js
@@ -1,4 +1,4 @@
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav, Dropdown } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
 
 
@@ -6,7 +6,7 @@ export default function NavBar({ loginFlag }) {
 
     return (
         <div id="home">
-            <Navbar bg="light" expand="lg">
+            <Navbar bg="light" expand="lg" style={{ zIndex: '500' }}>
                 <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
@@ -15,22 +15,33 @@ export default function NavBar({ loginFlag }) {
                             Home
                         </NavLink>
                         <Nav.Link href="#visualizer">Visulizer</Nav.Link>
-                        <NavLink className="nav-link" activeClassName="is-active" to="/chat_room">
-                            Chat Room
-                        </NavLink>
                         {
                             loginFlag ?
-                            <>
-                                <NavLink to="/account" className="btn btn-outline-primary mr-2 mb-2 mb-0">My Account</NavLink>
-                                <NavLink to="/logout" className="btn btn-primary mr-2 mb-2 mb-0">Logout</NavLink>
-                            </>
-                            :
-                            <>
-                                <NavLink to="/signIn" className="btn btn-outline-primary mr-2 mb-2 mb-0">Login</NavLink>
-                                <NavLink to="/signUp" className="btn btn-primary mr-2 mb-2 mb-0">Sign Up</NavLink>
-                            </>
+                                <>
+                                    <NavLink className="nav-link" activeClassName="is-active" to="/chat_room">
+                                        Chat Room
+                                    </NavLink>
+                                    <Dropdown>
+                                        <Dropdown.Toggle variant="light" id="dropdown-basic">
+                                            Algorithm
+                                        </Dropdown.Toggle>
+                                        <Dropdown.Menu style={{ left: '-50px' }}>
+                                            <Dropdown.Item as="button">
+                                                <NavLink to="/account" style={{ color: 'black', textDecoration: 'none' }}>My Account</NavLink>
+                                            </Dropdown.Item>
+                                            <Dropdown.Item as="button">
+                                                <NavLink to="/logout" style={{ color: 'black', textDecoration: 'none' }}>Logout</NavLink>
+                                            </Dropdown.Item>
+                                        </Dropdown.Menu>
+                                    </Dropdown>
+                                </>
+                                :
+                                <>
+                                    <NavLink to="/signIn" className="btn btn-outline-success mr-2 mb-2 mb-0">Login</NavLink>
+                                    <NavLink to="/signUp" className="btn btn-success mr-2 mb-2 mb-0">Sign Up</NavLink>
+                                </>
                         }
-                        
+
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
