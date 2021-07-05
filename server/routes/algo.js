9a24a1a60d733655ff0a032122c77f5673b1c15c
Mayukh Chakrabarti <Mayukh.chakrabarti@bhnetwork.com>
2021-07-05 23:21:24 +0530

    Api to get all algorithms

--- server/routes/algo.js
+++ server/routes/algo.js
@@ -4,7 +4,19 @@ const algorithms = require('../models/algorithms').algorithms;
 const favourites = require('../models/favourites').favourites;
 const route = express.Router();
 
+route.get('/getAll', async (req, res) => {
+    try {
+        const data = await algorithms.find({});
+        return res.json(data);
+    } catch(error) {
+        console.log('There has been an error while getAll: ' + error);
+        return res.jsnon({'error': 'Error while fetching data'});
+    }
+})
 
+// route.get('/add/fav', async (req, res) => {
+
+// })
 
 
 
